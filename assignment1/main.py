5a9a174544475b09be9d6bd7f8b84600679d5a3a
elieeshoagit1 <elieeshoagit1@gmail.com>
2021-10-21 11:02:10 -0400

    Abstracting by adding weights and others

--- assignment1/main.py
+++ assignment1/main.py
@@ -16,32 +16,42 @@ class User:
 
 # Takes in two user objects and outputs a float denoting compatibility
 def compute_score(user1, user2):
-    # YOUR CODE HERE
-    # user_obj['name'], user_obj['gender'],
-    #                         user_obj['preferences'], user_obj['gradYear'],
-    #                         user_obj['responses']
-    final_score = 0
+    grad_year_weight = 0.2
+    responses_weight = 0.8
 
+    # If a user is not in the other's preferences, the score is 0
     if not (user1.gender in user2.preferences): 
         return 0
 
     def grad_year_compatibility(user1, user2):
+        # Each gap in grad_years is mapped to a score out of 100
         dictionary = {
-            0:11,
-            1:9,
-            2:6,
-            3:3
+            0:100,
+            1:80,
+            2:65,
+            3:30
         }
         return dictionary[abs(user1.grad_year - user2.grad_year)]
 
     def responses_compatibility(user1, user2):
-        return 100 - sum(abs(user1.responses[i] - user2.responses[i]) \
-                         for i in range(len(user1.responses)))
+        # Take the sum of absolute differences between responses and subtract 
+        # that sum from the possible maximum. Then, 
+        # A response could be {0,...5}
+        max_difference = 5
+        responses_num = len(user1.responses)
 
-    final_score += (grad_year_compatibility(user1, user2) + \
-                    responses_compatibility(user1, user2)) * 100 / 111
+        max_score = max_difference * responses_num
+        scale = 100 / max_score
 
-    return grad_year_compatibility(user1, user2), responses_compatibility(user1, user2), final_score
+        compatibility_score = \
+            max_score - (sum(abs(user1.responses[i] - user2.responses[i]) \
+                                 for i in range(len(user1.responses))))
+        return compatibility_score * scale
+
+    final_score = grad_year_compatibility(user1, user2) * grad_year_weight + \
+                  responses_compatibility(user1, user2) * responses_weight
+
+    return round(final_score, 1)
 
 
 if __name__ == '__main__':
